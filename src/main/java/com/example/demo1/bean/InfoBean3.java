85d668f492197e2f7b9310e9157695f0470af497
fjx <1060422838@qq.com>
2021-12-12 22:04:02 +0800

    增加getter和setter

--- src/main/java/com/example/demo1/bean/InfoBean3.java
+++ src/main/java/com/example/demo1/bean/InfoBean3.java
@@ -1,7 +1,31 @@
 package com.example.demo1.bean;
 
 public class InfoBean3 {
-    public int year;
-    public String province;
-    public String num;
+    private int year;
+    private String province;
+    private String num;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
 }
